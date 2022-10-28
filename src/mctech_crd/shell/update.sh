e7a2ce7e475f1682c6e5c448d5b55bed55a412da
MC-Technology <>
2022-10-28 15:20:57 +0000

    Update from https://github.com/MC-Technology/cosmic-pi/commit/c1db9a461ac346fa274d81f42f0ebdb12128cb1b

--- src/mctech_crd/shell/update.sh
+++ src/mctech_crd/shell/update.sh
@@ -9,7 +9,7 @@ export GH_CREDENTIALS=$CREDENTIALS_DIR/gh_token.sh
 
 # export COSMIC_DEV_PACKAGE_URL=git+https://$GH_TOKEN@github.com/MC-Technology/mctech-crd.git
 
-if [ $OSTYPE == "darwin"* ]; then
+if [[ $OSTYPE == "darwin"* ]]; then
     # homebrew pyenv versions seem to be behind
     export COSMIC_PYTHON_VERSION=3.11.0rc2
     export COSMIC_VIRTUALENV=cosmic-3.11
@@ -17,6 +17,8 @@ else
     export COSMIC_PYTHON_VERSION=3.11.0
     export COSMIC_VIRTUALENV=cosmic-3.11
 fi
+echo "COSMIC_PYTHON_VERSION=$COSMIC_PYTHON_VERSION"
+echo "COSMIC_VIRTUALENV=$COSMIC_VIRTUALENV"
 
 DIR=$(dirname "$0")
 cd $DIR
