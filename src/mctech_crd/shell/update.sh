6405d2b97408b6b256f40f979118545e86d49e52
MC-Technology <>
2022-10-31 16:05:20 +0000

    v0.2.1

--- src/mctech_crd/shell/update.sh
+++ src/mctech_crd/shell/update.sh
@@ -8,6 +8,29 @@ export RELEASE=~/.cosmic/tmp/cosmic-latest-release
 
 # export COSMIC_DEV_PACKAGE_URL=git+https://$GH_TOKEN@github.com/MC-Technology/mctech-crd.git
 
+update_python_environment() {
+    # Should pass through if already installed
+    pyenv update
+
+    # This should match mctech-crd/.python-version
+    pyenv install $COSMIC_PYTHON_VERSION -s
+    pyenv virtualenv $COSMIC_PYTHON_VERSION $COSMIC_VIRTUALENV
+    PYENV_VERSION=$COSMIC_VIRTUALENV
+
+    # If now invalid version, use system (3.9.2)
+    VER=`python -V`
+    if [[ -z "${VER}" ]]; then
+        PYENV_VERSION=system
+    fi
+
+    # When run from shell this will activate pyenv virtualenv
+    export PYENV_VERSION=$PYENV_VERSION
+
+    # On boot, this will inform PYENV_VERSION set in bashrc
+    # this makes the 'cosmic' command available from the virtual environment
+    echo $PYENV_VERSION > $COSMIC_HOME/.python-version
+}
+
 download_latest_release() {
     mkdir -p $RELEASE
     rm -rf $RELEASE/*;
@@ -28,4 +51,5 @@ install_latest_release(){
     fi
 }
 
+update_python_environment
 install_latest_release
